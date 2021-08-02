d79c2da6576b7a5a2f89b4d0ba4285568f6cbc9e
CraftException <55949980+LiamFiedler@users.noreply.github.com>
2021-08-02 22:00:02 +0200

    Add Code View Script

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
-$(document).ready(function(){
-    const colorPalette = ['C0392B', 'E74C3C', 'D35400', 'E67E22', 'F39C12', 'F1C40F', '2ECC71', '27AE60',  '3498DB', '2980B9', '9B59B6', '8E44AD', '34495E', '2C3E50', '000000', '7F8C8D', '95A5A6', 'BDC3C7', 'ECf0F1', 'FFFFFF'],
-        forePalette = $('.fore-palette'),
-        backPalette = $('.back-palette'),
-        editor = $('.editor');
 
-    for (let i = 0; i < colorPalette.length; i++) {
+$(document).ready(function(){
+  $("#updateCode").click(function(){
+    console.log($("#viewCodeArea").text())
+  });
+  $('#viewCode').on('shown.bs.modal', function (e) {
+    $("#viewCodeArea").val($("#editor").html());
+  });
+
+  const colorPalette = ['C0392B', 'E74C3C', 'D35400', 'E67E22', 'F39C12', 'F1C40F', '2ECC71', '27AE60',  '3498DB', '2980B9', '9B59B6', '8E44AD', '34495E', '2C3E50', '000000', '7F8C8D', '95A5A6', 'BDC3C7', 'ECf0F1', 'FFFFFF'],
+      forePalette = $('.fore-palette'),
+      backPalette = $('.back-palette'),
+      editor = $('.editor');
+
+  for (let i = 0; i < colorPalette.length; i++) {
     forePalette.append('<a href="#" data-command="foreColor" data-value="' + '#' + colorPalette[i] + '" style="background-color:' + '#' + colorPalette[i] + ';" class="palette-item"></a>');
     backPalette.append('<a href="#" data-command="backColor" data-value="' + '#' + colorPalette[i] + '" style="background-color:' + '#' + colorPalette[i] + ';" class="palette-item"></a>');
   }
+
   $('.toolbar a').click(function(e) {
 		e.preventDefault();
-      const command = $(this).data('command');
+      const command = $(this).data('command');      
       if (command === 'h1' || command === 'h2' || command === 'p') {
       document.execCommand('formatBlock', false, command);
     }
@@ -30,14 +39,17 @@ $(document).ready(function(){
 			console.log(command + " " + url);
 			document.execCommand($(this).data('command'), false, url);
       // document.execCommand($(this).data('command') && 'enableObjectResizing', false, url);
-    } else document.execCommand($(this).data('command'), false, url);
+    } else document.execCommand($(this).data('command'));
   });
+
 	$('.editorAria img').click(function(){
       document.execCommand('enableObjectResizing', false);
 	});
+
 	$("#getHTML").click(function(){
         const editorId = $(this).attr('get-data');
         const html = $("#" + editorId).find('.editorAria').html();
         alert(html);
 	});
+
 });
